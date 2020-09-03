ac154e5d2c260f69b6ab55af95aaab21a2ace4da
Olha Peretiatko <multimediacentr2@gmail.com>
2020-09-03 15:43:22 +0300

    v

---
+++ lesson1/task2.py
@@ -0,0 +1,14 @@
+# ‘hello world’
+print('Hello world')
+
+# '145 765 675'
+print(145, 765, 675)
+
+# 'Occupy Mars!!!
+print('Occupy Mars', end='!!!')
+
+# 'Go'
+print(' Go!')
+
+# '987<123456<78865324'
+print(987, 123456, 78865324, sep='<')
---
+++ lesson1/task3.py
@@ -0,0 +1,29 @@
+# 'O'
+print('#########')
+print('#', '#', sep='\t\t')
+print('#', '#', sep='\t\t')
+print('#', '#', sep='\t\t')
+print('#########')
+
+# 'space'
+print('')
+
+# 'H'
+print('')
+print('#', '#', sep='\t')
+print('#', '#', sep='\t')
+print('#########')
+print('#', '#', sep='\t')
+print('#', '#', sep='\t')
+
+# 'space'
+print('')
+
+# 'space'
+print('')
+
+# 'Y'
+print('#', '#', sep='\t')
+print('', '', '#', '', '#',)
+print('', '','', '#',)
+print('', '','', '#',)
